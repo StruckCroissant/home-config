1f9957623eae35e0d94cc06d83a8ffddc4f67f69
StruckCroissant <dakotavaughn5592@gmail.com>
2024-10-06 21:21:59 -0500

    init commit

---
+++ home.nix
@@ -0,0 +1,191 @@
+{ config, pkgs, lib, ... }:
+let
+ username = "struckcroissant";
+ homeDir = "/home/${username}";
+in
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "${username}";
+  home.homeDirectory = "${homeDir}";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+    nerdfonts
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/struckcroissant/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  home.shellAliases = {
+    "hm" = "home-manager";
+    "ls" = "ls --color=auto";
+    "grep" = "grep --color=auto";
+  };
+
+  #home.file."${homeDir}/.config/starship.toml" = {
+  #  enable = true;
+  #  source = "${homeDir}/.config/home-manager/config/starship.toml";
+  #};
+  
+  programs.home-manager.enable = true;
+
+  programs.bash = {
+    enable = true;
+    bashrcExtra = ''
+    ${homeDir}/.config/home-manager/config/.bashrc
+    '';
+    enableCompletion = true;
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "StruckCroissant";
+    userEmail = "dakotavaughn5592@gmail.com";
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    vimAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      lightline-vim
+    ];
+    extraConfig = ''
+      set rnu
+      set number
+    '';
+  };
+
+  programs.starship = {
+    enable = true;
+    enableBashIntegration = true;
+    settings = {
+      format = lib.concatStrings [
+        "[░▒▓](#a3aed2)"
+        "[  ](bg:#a3aed2 fg:#090c0c)"
+        "[](bg:#769ff0 fg:#a3aed2)"
+        "$directory"
+        "[](fg:#769ff0 bg:#394260)"
+        "$git_branch"
+        "$git_status"
+        "[](fg:#394260 bg:#212736)"
+        "$nodejs"
+        "$rust"
+        "$golang"
+        "$php"
+        "[](fg:#212736 bg:#1d2230)"
+        "$time"
+        "[ ](fg:#1d2230)"
+        "\n$character"
+      ];
+      directory = {
+        style = "fg:#e3e5e5 bg:#769ff0";
+        format = "[ $path ]($style)";
+        truncation_length = 3;
+        truncation_symbol = "…/";
+      };
+      directory.substitutions = {
+        "Documents" = "󰈙 ";
+        "Downloads" = " ";
+        "Music" = " ";
+        "Pictures" = " ";
+      };
+      git_branch = {
+        symbol = "";
+        style = "bg:#394260";
+        format = "[[ $symbol $branch ](fg:#769ff0 bg:#394260)]($style)";
+      };
+      git_status = {
+        style = "bg:#394260";
+        format = "[[($all_status$ahead_behind )](fg:#769ff0 bg:#394260)]($style)";
+      };
+      nodejs = {
+        symbol = "";
+	style = "bg:#212736";
+	format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+      };
+      rust = {
+        symbol = "";
+        style = "bg:#212736";
+        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+      };
+      golang = {
+        symbol = "";
+        style = "bg:#212736";
+        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+      };
+      php = {
+        symbol = "";
+        style = "bg:#212736";
+        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+      };
+      time = {
+        disabled = false;
+        time_format = "%R"; # Hour:Minute Format
+        style = "bg:#1d2230";
+        format = "[[  $time ](fg:#a0a9cb bg:#1d2230)]($style)";
+      };
+    };
+  };
+
+  programs.dircolors = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+}
+
