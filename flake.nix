7dd82eefd4bcfd8570f329aa21481dea980b4467
Dakota <dvaughn@xes-inc.com>
2024-10-08 21:17:09 -0500

    Implemented (hacky & probably gross) flake approach

--- flake.nix
+++ flake.nix
@@ -1,29 +1,14 @@
 {
-  description = "Home Manager configuration of struckcroissant";
+  description = "Home manager base config flake";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    core.url = "path:core";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations."struckcroissant" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
-    };
+   outputs = { self, core, ... }: core.generate {
+     username = "struckcroissant";
+     userName = "Dakota Vaughn";
+     host = "Hub";
+     email = "32440863+StruckCroissant@users.noreply.github.com";
+   };
 }
