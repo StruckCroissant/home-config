9d9938f49b41fa676020ba559913198b032f5531
Dakota Vaughn <32440863+StruckCroissant@users.noreply.github.com>
2024-10-08 21:54:54 -0500

    Basic variable setup

--- flake.nix
+++ flake.nix
@@ -1,8 +1,7 @@
 {
-  description = "Home Manager configuration of Dakota";
+  description = "Home manager base config flake";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
@@ -10,19 +9,20 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       lib = pkgs.lib;
       config = pkgs.config;
       stateVersion = "24.05";
-      metadata = rec {
-        userName = "Dakota Vaughn";
-        username = "struckcroissant";
-        homeDirectory = "/home/${username}";
-        host = "Hub";
-        email = "32440863+StruckCroissant@users.noreply.github.com";
+      metadata = {
+        inherit (import ./variables.nix)
+	  userName
+	  username
+	  homeDirectory
+	  host
+	  email;
       };
       home = import ./home.nix { 
         inherit metadata pkgs lib stateVersion config; 
