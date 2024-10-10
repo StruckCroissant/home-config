779149405a689247a849eee47892dc35f389e2cf
StruckCroissant <32440863+StruckCroissant@users.noreply.github.com>
2024-10-09 21:08:29 -0500

    Revert "Implemented (hacky & probably gross) flake approach"
    This reverts commit 7dd82eefd4bcfd8570f329aa21481dea980b4467.

--- flake.nix
+++ flake.nix
@@ -1,14 +1,29 @@
 {
-  description = "Home manager base config flake";
+  description = "Home Manager configuration of struckcroissant";
 
   inputs = {
-    core.url = "path:core";
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-   outputs = { self, core, ... }: core.generate {
-     username = "struckcroissant";
-     userName = "Dakota Vaughn";
-     host = "Hub";
-     email = "32440863+StruckCroissant@users.noreply.github.com";
-   };
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."struckcroissant" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
 }
--- home.nix
+++ home.nix
@@ -1,8 +1,7 @@
-{ config, pkgs, lib, metadata, stateVersion, ... }:
+{ config, pkgs, lib, ... }:
 let
- username = metadata.username;
- homeDir = metadata.homeDirectory;
- userName = metadata.userName;
+ username = "struckcroissant";
+ homeDir = "/home/${username}";
 in
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -10,14 +9,64 @@ in
   home.username = "${username}";
   home.homeDirectory = "${homeDir}";
 
-  home.stateVersion = stateVersion;
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
     nerdfonts
   ];
 
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/struckcroissant/etc/profile.d/hm-session-vars.sh
+  #
   home.sessionVariables = {
     EDITOR = "nvim";
   };
@@ -28,22 +77,20 @@ in
     "grep" = "grep --color=auto";
   };
 
-  programs.home-manager = {
-    enable = true;
-  };
+  programs.home-manager.enable = true;
 
   programs.bash = {
     enable = true;
     bashrcExtra = ''
-    ${homeDir}/.config/home-manager/core/config/.bashrc
+    ${homeDir}/.config/home-manager/config/.bashrc
     '';
     enableCompletion = true;
   };
 
   programs.git = {
     enable = true;
-    userName = metadata.userName;
-    userEmail = metadata.email;
+    userName = "StruckCroissant";
+    userEmail = "32440863+StruckCroissant@users.noreply.github.com";
   };
 
   programs.neovim = {
@@ -104,8 +151,8 @@ in
       };
       nodejs = {
         symbol = "";
-	      style = "bg:#212736";
-	      format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+	style = "bg:#212736";
+	format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
       };
       rust = {
         symbol = "";
@@ -141,3 +188,4 @@ in
     mouse = true;
   };
 }
+
