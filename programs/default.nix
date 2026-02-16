d90effd3a8938f44e84ff7155bc5339478c24874
Kota <32440863+StruckCroissant@users.noreply.github.com>
2026-02-16 10:25:41 -0600

    WIP fixing catppuccin

--- programs/default.nix
+++ programs/default.nix
@@ -177,7 +177,21 @@ in
       plugins = with pkgs.tmuxPlugins; [
         sensible
         vim-tmux-navigator
-        catppuccin
+        {
+          plugin = catppuccin;
+          extraConfig = ''
+            set -g @catppuccin_flavor 'mocha' # or frappe, macchiato, latte
+            set -g @catppuccin_window_status_style "rounded"
+            
+            # These lines are now required to actually show the modules
+            set -g status-right-length 100
+            set -g status-left-length 100
+            set -g status-left ""
+            set -g status-right "#{E:@catppuccin_status_application}"
+            set -ag status-right "#{E:@catppuccin_status_session}"
+            set -ag status-right "#{E:@catppuccin_status_uptime}"
+          '';
+        }
       ];
     };
   };
